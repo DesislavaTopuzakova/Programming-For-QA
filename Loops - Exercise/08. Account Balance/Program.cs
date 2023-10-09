36de4ff73d20ab295942c7d95929a01417df509e
Desislava Topuzakova <34713586+DesislavaTopuzakova@users.noreply.github.com>
2023-10-09 18:47:32 +0300

    08. Account Balance

---
+++ Loops - Exercise/08. Account Balance/Program.cs
@@ -0,0 +1,31 @@
+﻿//повтаряме: въвеждаме данни
+//стоп: input == "End"
+//продължаваме: input != "End"
+double balance = 0; //баланс на сметката
+
+string input = Console.ReadLine(); //входни данни: "500" или "End"
+
+while (input != "End")
+{
+    //входни данни са число под формата на текст "500"
+    //"500" -> double 500
+    double money = double.Parse(input); //пари, които внасяме / теглим
+    if (money >= 0)
+    {
+        //внасяне
+        balance += money;
+        Console.WriteLine($"Increase: {money:F2}");
+    }
+    else //money < 0
+    {
+        //теглим
+        balance -= Math.Abs(money);
+        Console.WriteLine($"Decrease: {Math.Abs(money):F2}");
+    }
+
+
+    input = Console.ReadLine();
+}
+
+//входни данни == "End"
+Console.WriteLine($"Balance: {balance:F2}");
