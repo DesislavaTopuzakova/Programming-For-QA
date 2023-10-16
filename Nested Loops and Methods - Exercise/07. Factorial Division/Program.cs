1d1d72ca24057a29f2795d94157c0378fadea649
Desislava Topuzakova <34713586+DesislavaTopuzakova@users.noreply.github.com>
2023-10-16 17:11:45 +0300

    07. Factorial Division

---
+++ Nested Loops and Methods - Exercise/07. Factorial Division/Program.cs
@@ -0,0 +1,27 @@
+﻿int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+
+//1. факториел на първото число
+int factFirstNumber = CalculateFactorial(firstNumber);
+
+
+//2. факториел на второто число
+int factSecondNumber = CalculateFactorial(secondNumber);
+
+
+//3. факториел на първото число / факториел на второто число
+Console.WriteLine(factFirstNumber / factSecondNumber);
+
+
+//метод, който изчислява и връща стойността на факториела
+static int CalculateFactorial(int number)
+{
+	//5! = 1 * 2 * 3 * 4 * 5
+	int fact = 1; //факториел на числото
+	for (int i = 1; i <= number; i++)
+	{
+		fact = fact * i;
+	}
+    //изчислен факториел в fact
+    return fact;
+}
