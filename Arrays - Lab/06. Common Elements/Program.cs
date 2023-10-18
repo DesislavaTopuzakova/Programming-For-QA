67e7a8d07c2451ece464046d85f1616d4b3f98ca
Desislava Topuzakova <34713586+DesislavaTopuzakova@users.noreply.github.com>
2023-10-18 17:42:45 +0300

    Update Program.cs

--- Arrays - Lab/06. Common Elements/Program.cs
+++ Arrays - Lab/06. Common Elements/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] firstArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int[] secondArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+
+foreach (int number in firstArray)
+{
+    foreach (int number2 in secondArray)
+    {
+        if (number == number2)
+        {
+            //общ елемент, който го има в двата масива
+            Console.Write(number + " ");
+            break;
+        }
+    }
+}
+
